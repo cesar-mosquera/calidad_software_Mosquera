cabc7f30efd4b302fbcaa8d1103ed05a777d2a58
cesar-mosquera <juliomosquera252@gmail.com>
2026-06-01 12:42:05 -0500

    CLASE 01-06-2026

---
+++ MODULO2/jest/matchers/toContain/toContainDemo.test.js
@@ -0,0 +1,28 @@
+﻿const { obtenerFrutas, contienePalabra, agregarElemento } = require('./toContainDemo');
+describe('toContain matcher', () => {
+    test('Caso Exitoso: elemento dentro de array', () => {
+        const frutas = obtenerFrutas();
+        expect(frutas).toContain('pera');
+    });
+
+
+
+    test('Caso Exitoso: palabra dentro de texto', () => {
+        expect(contienePalabra('Jest es increÃ­ble', 'increÃ­ble')).toBe(true);
+    });
+
+
+
+    test('Caso Fallido: elemento no encontrado o tipo incorrecto', () => {
+        const frutas = obtenerFrutas();
+        expect(frutas).not.toContain('banana');
+        expect(() => contienePalabra(123, 'texto')).toThrow('Ambos argumentos deben ser strings');
+    });
+
+
+
+    test('Caso Fallido: agregarElemento con valores invÃ¡lidos', () => {
+        expect(() => agregarElemento({}, 'x')).toThrow('lista debe ser un array');
+        expect(() => agregarElemento([], '')).toThrow('elemento no puede estar vacÃ­o');
+    });
+});
